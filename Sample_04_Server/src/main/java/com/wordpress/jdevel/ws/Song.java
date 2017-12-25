54565db7cd39c73ceb26962fc986a0a7f8247f2a
Lebedev Arthur <lebedev_a@parfum.spb.ru>
2017-12-25 03:12:12 +0300

    0

--- Sample_04_Server/src/main/java/com/wordpress/jdevel/ws/Song.java
+++ Sample_04_Server/src/main/java/com/wordpress/jdevel/ws/Song.java
@@ -9,43 +9,30 @@ public class Song implements Serializable {
 
     public Song() {
     }
-
     public Song(String fileName, String artist, int size) {
-
         this.fileName = fileName;
-
         this.artist = artist;
-
         this.size = size;
     }
 
     public String getArtist() {
-
         return artist;
     }
-
     public void setArtist(String artist) {
-
         this.artist = artist;
     }
 
     public String getFileName() {
-
         return fileName;
     }
-
     public void setFileName(String fileName) {
-
         this.fileName = fileName;
     }
 
     public int getSize() {
-
         return size;
     }
-
     public void setSize(int size) {
-
         this.size = size;
     }
 }
