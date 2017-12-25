4f241e8498d558cdd863f8a11a392f7e189afdb5
Lebedev Arthur <lebedev_a@parfum.spb.ru>
2017-12-26 02:57:36 +0300

    0

---
+++ Lesson_08/src/ru/javabegin/training/webservices/main/Start.java
@@ -0,0 +1,15 @@
+package ru.javabegin.training.webservices.main;
+
+import ru.javabegin.training.webservices.parsers.DomParser;
+import ru.javabegin.training.webservices.parsers.SaxParser;
+import ru.javabegin.training.webservices.parsers.StaxParser;
+
+public class Start {
+    
+    public static void main(String[] args) {
+        String xmlPath = "people2.xml";
+//        new DomParser().parse(xmlPath);
+        new SaxParser().parse(xmlPath);
+//         new StaxParser().parse(xmlPath);
+    }
+}
