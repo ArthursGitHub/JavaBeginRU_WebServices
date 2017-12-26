8255ad881c7f6b8f5f91efaf12b3e11cf8d693c0
Lebedev Arthur <lebedev_a@parfum.spb.ru>
2017-12-26 17:08:38 +0300

    0

---
+++ Lesson_11/readme.txt
@@ -0,0 +1,2 @@
+You can test the deployed web service by accessing the generated WSDL (Web Service Definition Language) document via this URL
+http://localhost:8888/ws/server?wsdl
---
+++ Lesson_11/src/main/java/com/mkyong/ws/ServerInfo.java
@@ -0,0 +1,13 @@
+package com.mkyong.ws;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
+public class ServerInfo {
+
+    @WebMethod
+    public String getIpAddress() {
+        return "10.10.10.10";
+    }
+}
